cf924d3f10bc2e657cdc6222fa24aa10c09c39ba
brandon.yang <brandon.gh.yang@pccw.com>
2020-08-11 10:55:57 +0800

    created new branch;

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-import App from "./App.vue";
-import vueSpatialNavigation from "./lib/vueSpatialNavigation";
-Vue.config.productionTip = false;
-
-Vue.use(vueSpatialNavigation);
-new Vue({
-  render: (h) => h(App)
-}).$mount("#app");
